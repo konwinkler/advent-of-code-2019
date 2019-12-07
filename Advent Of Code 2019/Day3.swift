6250470775123445f7f78ff8ec6a31c75bc8e3dd
konwinkler <konrad.winkler@willowtreeapps.com>
2019-12-06 22:31:46 -0500

    Solution day 3
    * tests for answer
    * day 3 code with enum and struct
    * uses set for faster comparison than array

--- Advent Of Code 2019/Day3.swift
+++ Advent Of Code 2019/Day3.swift
@@ -12,13 +12,95 @@ class Day3 {
     
     func closestCrossing(_ firstWirePath: [String], _ secondWirePath: [String]) -> Int {
         // parse paths
+        var firstPath: Set<Point> = []
+        var start = Point(0, 0)
+        for pathValue in firstWirePath {
+            let newPoints = self.path(from: start, along: pathValue)
+            firstPath.formUnion(newPoints)
+            start = newPoints.last ?? Point(0, 0)
+        }
         
-        // create list of corrdinates
+        var secondPath: Set<Point> = []
+        start = Point(0, 0)
+        for pathValue in secondWirePath {
+            let newPoints = self.path(from: start, along: pathValue)
+            secondPath.formUnion(newPoints)
+            start = newPoints.last ?? Point(0, 0)
+        }
         
         // find all intersections
+        let intersections = firstPath.intersection(secondPath)
         
         // return intersection with shortest manhattan distance
-        return 0
+        var intersationDistances = intersections.map({
+            $0.manhatten
+        })
+        
+        intersationDistances.sort(by: <)
+        
+        return intersationDistances.first ?? 0
     }
     
+    func path(from start: Point, along path: String) -> [Point] {
+        var result: [Point] = []
+        var x = start.x
+        var y = start.y
+        
+        
+        let direction = Direction.init(rawValue: String(path.prefix(1))) ?? Direction.up
+        
+        let length: Int = Int(String(path.suffix(path.count - 1))) ?? 0
+        
+        for _ in 1 ... length {
+            x += direction.x
+            y += direction.y
+            result.append(Point(x, y))
+        }
+        
+        return result
+    }
+    
+}
+
+enum Direction: String {
+    case up = "U"
+    case down = "D"
+    case left = "L"
+    case right = "R"
+    
+    var x: Int {
+        switch self {
+        case .left:
+            return -1
+        case .right:
+            return 1
+        default:
+            return 0
+        }
+    }
+    
+    var y: Int {
+        switch self {
+        case .down:
+            return -1
+        case .up:
+            return 1
+        default:
+            return 0
+        }
+    }
+}
+
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+    
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    var manhatten: Int {
+        return abs(self.x) + abs(self.y)
+    }
 }
--- Tests/Day3Tests.swift
+++ Tests/Day3Tests.swift
@@ -11,9 +11,31 @@ import XCTest
 class Day3Tests: XCTestCase {
     
     let day = Day3()
+    let input = InputDay3()
 
     func testSample() {
         XCTAssertEqual(day.closestCrossing(["R75","D30","R83","U83","L12","D49","R71","U7","L72"], ["U62","R66","U55","R34","D71","R55","D58","R83"]), 159)
+        XCTAssertEqual(day.closestCrossing(["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"], ["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]), 135)
+    }
+    
+    func testAnswer() {
+        let answer = day.closestCrossing(input.getFirst(), input.getSecond())
+        XCTAssertEqual(answer, 2193)
+    }
+    
+    func testPath() {
+        let start = Point(0, 0)
+        XCTAssertEqual(day.path(from: start, along: "U1"), [Point(0, 1)])
+        XCTAssertEqual(day.path(from: start, along: "U2"), [Point(0, 1), Point(0, 2)])
+        XCTAssertEqual(day.path(from: start, along: "D3"), [Point(0, -1), Point(0, -2), Point(0, -3)])
+    }
+    
+    func testManhatten() {
+        XCTAssertEqual(Point(0, 0).manhatten, 0)
+        XCTAssertEqual(Point(0, 1).manhatten, 1)
+        XCTAssertEqual(Point(3, 1).manhatten, 4)
+        XCTAssertEqual(Point(-3, 1).manhatten, 4)
+        XCTAssertEqual(Point(3, -4).manhatten, 7)
     }
 
 }
